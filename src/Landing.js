ac966677e02741fe22f5bf3a2c1b3656e131b465
follmannkai <follmannkai@gmail.com>
2021-10-05 09:44:19 +0200

    updated2

--- src/Landing.js
+++ src/Landing.js
@@ -5,23 +5,26 @@ export default function Landing() {
       <h1>What we do</h1>
       <p className="p1">
         QuanTUM is the first Hedgefund society founded at Germanys #1
-        university. TUM has the brightest minds in fields including Mathematics,
-        Computer Science, Engineering, Physics and Business. When founding
-        QuanTUM, we had three things in mind:
+        university. <br />
+        TUM has the brightest minds in fields including Mathematics, Computer
+        Science, Engineering, Physics and Business.
+        <br /> When founding QuanTUM, we had three things in mind:
       </p>
       <h3>1. Your major does not matter.</h3>
       <p>We believe that true diversity comes from having diverse minds. </p>
 
       <h3>2. You do not need any prior knowledge in finance</h3>
       <p>
-        Breakthroughs happen through novel approaches; which often come from
-        people who have not worked in the respective field before.
+        Breakthroughs happen through novel approaches; <br />
+        which often come from people who have not worked in the respective field
+        before.
       </p>
       <h3>3. Your age does not matter.</h3>
       <p>
-        You could be in your first year of your studies or in a PhD programme.
-        We are interested in your personality. Still, having expertise in
-        certain fields might help significantlz.
+        You could be in the first year of your studies or in a Ph.D. program.
+        <br /> We are interested in your personality.
+        <br /> Still, having expertise in certain fields might help
+        significantly.
       </p>
     </div>
   );
